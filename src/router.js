79dcadbab729114d42127ef848355792b49c69fe
MAC <nwokikesamson@gmail.com>
2019-04-14 12:58:12 +0100

    removed unneeded components

--- src/router.js
+++ src/router.js
@@ -10,10 +10,10 @@ export default new Router({
       component: () => import('@/layouts/Default.vue'),
       children: [ 
         { path: '', name: 'Landing', component: () => import('@/views/Components.vue') },
-        { path: '/landing', name: 'landing', component: () => import('@/views/Landing.vue') },
-        { path: '/login', name: 'login', component: () => import('@/views/Login.vue') },
-        { path: '/register', name: 'register', component: () => import('@/views/Register.vue') },
-        { path: '/profile', name: 'submitted', component: () => import('@/views/Profile.vue') }
+        // { path: '/landing', name: 'landing', component: () => import('@/views/Landing.vue') },
+        // { path: '/login', name: 'login', component: () => import('@/views/Login.vue') },
+        // { path: '/register', name: 'register', component: () => import('@/views/Register.vue') },
+        // { path: '/profile', name: 'submitted', component: () => import('@/views/Profile.vue') }
       ]
     }
 
