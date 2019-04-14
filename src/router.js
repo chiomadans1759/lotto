679fe32ed342d53828631783bfdc626c03662356
omadamsel1759 <mac@MACs-MacBook-Pro-3.local>
2019-04-14 21:02:46 +0100

    Completed the Dashboard Result View

--- src/router.js
+++ src/router.js
@@ -14,7 +14,8 @@ export default new Router({
         { path: '', name: 'landing', component: () => import('@/views/Landing.vue') }, 
         { path: '/login', name: 'login', component: () => import('@/views/Login.vue') },
         { path: '/not-found', name: 'not-found', component: () => import('@/views/NotFound.vue') },
-        { path: '/dashboard', name: 'not-found', component: () => import('@/views/Dashboard.vue') }
+        { path: '/dashboard', name: 'dashboard', component: () => import('@/views/Dashboard.vue') },
+        { path: '/dashboard-result', name: 'dashboard-result', component: () => import('@/views/DashboardResult.vue') }
       ]
     }
   ]
