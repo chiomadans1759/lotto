a03116d763802b1be271e7e84c731f033f0a1a6c
MAC <nwokikesamson@gmail.com>
2019-04-15 22:46:52 +0100

    initial setup

---
+++ src/layouts/WalletLayout.vue
@@ -0,0 +1,53 @@
+<template>
+<div id="wallet">
+    <!-- Sidebar -->
+   <SideNav/>
+
+    <div id="content">
+      <div class="page">
+        <router-view></router-view>     
+      </div>
+        <!-- <Footer/> -->
+    </div>
+
+</div>
+</template>
+
+<script>
+import Header from "@/components/AppHeader";
+import Footer from "@/components/AppFooter";
+import SideNav from "@/components/WalletSideNav";
+export default {
+  name: "components",
+  components: {
+    Header,
+    Footer,
+    SideNav
+  }
+};
+</script>
+<style scoped>
+#wallet{
+  background: #E5E5E5;
+  height: 100vh;
+  /* overflow-y: scroll; */
+}
+#wallet #content{
+  width: 100%;
+  margin-left: 310px;
+  height: 100vh;
+  overflow-y: scroll;
+}
+#wallet .header{
+  background: #fff;
+}
+#wallet .page-title{
+  background: transparent;
+}
+#wallet .page{
+  background: #fff;
+  width: 75%;
+  overflow-y: scroll;
+}
+
+</style>
